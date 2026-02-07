b9cd0844bf406dfb5b9a1f22b22113133ea25e20
adarshsingh888 <singh273013@gmail.com>
2026-02-07 16:08:51 +0530

    Uses ssh keys

--- DMA/memory.c
+++ DMA/memory.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     printf("Git and github testing.\n");
     printf("This is git sync learning and testing repository.\n");
+    printf("Git ssh key testing.\n");
     return 0;
-
 }
